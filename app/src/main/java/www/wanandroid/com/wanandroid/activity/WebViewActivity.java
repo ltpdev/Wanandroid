218629f251329a9ab5ca9611c3cf5290a667fc61
ltpdev <1776494277@qq.com>
2018-10-11 20:54:00 +0800

    实现列表向上滑动时，悬浮按钮隐藏，下滑显示

--- app/src/main/java/www/wanandroid/com/wanandroid/activity/WebViewActivity.java
+++ app/src/main/java/www/wanandroid/com/wanandroid/activity/WebViewActivity.java
@@ -3,6 +3,7 @@ package www.wanandroid.com.wanandroid.activity;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -12,6 +13,9 @@ import android.widget.ProgressBar;
 
 import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,9 +32,11 @@ public class WebViewActivity extends BaseActivity {
     WebView webview;
     @Autowired(name = Constant.KEY_WEBVIEW)
     String htmlUrl;
+    private List<String> loadHistoryUrls = new ArrayList<>();
 
     @Override
     protected void init() {
+        initToolbar();
         webview.getSettings().setJavaScriptEnabled(true);
         //设置自适应屏幕，两者合用
         webview.getSettings().setUseWideViewPort(true);//将图片调整到合适webview的大小
@@ -41,24 +47,45 @@ public class WebViewActivity extends BaseActivity {
             webview.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         }
         webview.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                webview.loadUrl(url);
-                return true;
-            }
-        });
+                                     @Override
+                                     public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                                         webview.loadUrl(url);
+                                         return true;
+                                     }
+
+                                     @Override
+                                     public void onPageFinished(WebView view, String url) {
+                                         super.onPageFinished(view, url);
+                                         loadHistoryUrls.add(url);
+                                     }
+                                 }
+
+        );
         webview.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public void onProgressChanged(WebView view, int newProgress) {
-                if (newProgress == 100) {
-                    pb.setVisibility(View.GONE);
-                } else {
-                    pb.setVisibility(View.VISIBLE);
-                    pb.setProgress(newProgress);
-                }
-            }
-        });
+                                       @Override
+                                       public void onProgressChanged(WebView view, int newProgress) {
+                                           if (newProgress == 100) {
+                                               pb.setVisibility(View.GONE);
+                                           } else {
+                                               pb.setVisibility(View.VISIBLE);
+                                               pb.setProgress(newProgress);
+                                           }
+                                       }
+
+                                       @Override
+                                       public void onReceivedTitle(WebView view, String title) {
+                                           super.onReceivedTitle(view, title);
+                                           getSupportActionBar().setTitle(title);
+                                       }
+                                   }
+
+        );
         webview.loadUrl(htmlUrl);
+    }
+
+    private void initToolbar() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("正在加载.....");
     }
 
     @Override
@@ -69,10 +96,31 @@ public class WebViewActivity extends BaseActivity {
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK && webview.canGoBack()) {
-            webview.goBack();
-            return true;
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (webview.canGoBack()) {
+                if (loadHistoryUrls.size() > 1) {
+                    //重新加载之前的页面,这里为了让标题也能正常显示
+                    String url = loadHistoryUrls.get(loadHistoryUrls.size() - 2);
+                    loadHistoryUrls.remove(loadHistoryUrls.size() - 1);
+                    if (loadHistoryUrls.size() > 0) {
+                        loadHistoryUrls.remove(loadHistoryUrls.size() - 1);
+                    }
+                    webview.loadUrl(url);
+                    return true;
+                }
+            }
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
